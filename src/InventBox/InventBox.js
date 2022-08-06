1fd3a048c5cd1947fdd35cff9d04ca6aeb99c9a1
Maxim Kirshin <meowto16@gmail.com>
2022-08-07 02:18:39 +0500

    Lesson: 3. InventBox. Adds colors to triangle

--- src/InventBox/InventBox.js
+++ src/InventBox/InventBox.js
@@ -22,19 +22,33 @@ if (!gl) {
  */
 
 const vertexData = [
-  0, 1, 0,
-  1, -1, 0,
-  -1, -1, 0
+  0, 1, 0, // V1. position
+  1, -1, 0, // V2. position
+  -1, -1, 0 // V3. position
 ]
 
-const buffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+const colorData = [
+  1, 0, 0, // V1.color
+  0, 1, 0, // V2.color
+  0, 0, 1 //V3.color
+]
+
+const positionBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW)
+
+const colorBuffer = gl.createBuffer()
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW)
 
 const vertexShader = gl.createShader(gl.VERTEX_SHADER)
 gl.shaderSource(vertexShader, `
 attribute vec3 position;
+attribute vec3 color;
+varying vec3 vColor;
+
 void main() {
+  vColor = color;
   gl_Position = vec4(position, 1);
 }
 `)
@@ -42,8 +56,12 @@ gl.compileShader(vertexShader)
 
 const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
 gl.shaderSource(fragmentShader, `
+precision mediump float;
+
+varying vec3 vColor;
+
 void main() {
-  gl_FragColor = vec4(1, 0, 0, 1);
+  gl_FragColor = vec4(vColor, 1);
 }
 `)
 gl.compileShader(fragmentShader)
@@ -51,11 +69,18 @@ gl.compileShader(fragmentShader)
 const program = gl.createProgram()
 gl.attachShader(program, vertexShader)
 gl.attachShader(program, fragmentShader)
+
 gl.linkProgram(program)
 
 const positionLocation = gl.getAttribLocation(program, `position`)
 gl.enableVertexAttribArray(positionLocation)
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
 gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0)
+
+const colorLocation = gl.getAttribLocation(program, `color`)
+gl.enableVertexAttribArray(colorLocation)
+gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0)
 
 gl.useProgram(program)
 gl.drawArrays(gl.TRIANGLES, 0, 3)
