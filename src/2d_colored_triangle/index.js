35e862def119c7905ed180c139afaa50c8ba2266
Maxim Kirshin <meowto16@gmail.com>
2022-09-02 01:23:18 +0300

    WebGLAcademy: 2D Colored triangle (Completed). Drew two triangles...

--- src/2d_colored_triangle/index.js
+++ src/2d_colored_triangle/index.js
@@ -7,7 +7,8 @@ if (!gl) {
   throw new Error('WebGL is not supported')
 }
 
-webglUtils.resizeCanvasToDisplaySize(canvas)
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
 
 const program = webglUtils.createProgramFromScripts(gl, ['vertex-shader-2d', 'fragment-shader-2d'])
 
@@ -22,19 +23,36 @@ gl.useProgram(program)
 // == TRIANGLE == //
 // POINTS
 const triangle_vertex = [
-  -1, -1, // first corner, bottom left
-  0, 0, 1,
-  1, -1, // bottom right of the viewport
-  1, 1, 0,
-  1, 1, // top right of the viewport
-  1, 0, 0
+  // 0 index
+  -1, -1, // position. first corner, bottom left (x, y)
+  0, 0, 1, // color. blue
+
+  // 1 index
+  1, -1, // position. bottom right of the viewport (x, y)
+  0, 1, 0, // color. red + green
+
+  // 2 index
+  1, 1, // position. top right of the viewport (x, y)
+  1, 0, 0, // color. red
+
+  // 3 index
+  -1, 1, // position. top left of the viewport (x, y)
+  0, 1, 0, // color red + green
+
+  // 4 index
+  1, 1, // position. top right of the viewport (x, y)
+  1, 0, 0, // color. red
+
+  // 5 index
+  -1, -1, // position. first corner, bottom left (x,y)
+  0, 0, 1, // color blue
 ]
 const TRIANGLE_VERTEX = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, TRIANGLE_VERTEX)
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangle_vertex), gl.STATIC_DRAW)
 
 // FACES
-const triangle_faces = [0, 1, 2]
+const triangle_faces = [0, 1, 2, 3, 4, 5]
 const TRIANGLE_FACES = gl.createBuffer()
 gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES)
 gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(triangle_faces), gl.STATIC_DRAW)
@@ -52,7 +70,7 @@ const animate = () => {
   gl.vertexAttribPointer(_color, 3, gl.FLOAT, false, 4 * (2 + 3), 2 * 4)
 
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, TRIANGLE_FACES)
-  gl.drawElements(gl.TRIANGLES, 3, gl.UNSIGNED_SHORT, 0)
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
   gl.flush()
 
   requestAnimationFrame(animate)
